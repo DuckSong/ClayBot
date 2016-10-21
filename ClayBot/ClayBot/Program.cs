d5cda55bfc11616f0f1835d0b4fc5ca016e92f11
Chaewon Lee <chris.chaewon.lee@gmail.com>
2016-10-20 18:02:28 -0700

    added error logging

--- ClayBot/ClayBot/Program.cs
+++ ClayBot/ClayBot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ClayBot
@@ -12,14 +13,35 @@ namespace ClayBot
         {
             string DataDirectory = Path.GetDirectoryName(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), Static.CONFIG_PATH));
             if (!Directory.Exists(DataDirectory)) Directory.CreateDirectory(DataDirectory);
+            
+            Application.ThreadException += new ThreadExceptionEventHandler(ThreadExceptionHandler);
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(UnhandledExceptionHandler);
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             MainForm mainForm = new MainForm();
+
             if (mainForm.Initialize())
             {
                 Application.Run(mainForm);
+            }
+        }
+
+        static void ThreadExceptionHandler(object sender, ThreadExceptionEventArgs e)
+        {
+            using (StreamWriter errorWriter = new StreamWriter(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "error.log"), false))
+            {
+                errorWriter.Write(e.Exception.ToString());
+            }
+        }
+
+        static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            using (StreamWriter errorWriter = new StreamWriter(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "error.log"), false))
+            {
+                errorWriter.Write(e.ExceptionObject.ToString());
             }
         }
     }
